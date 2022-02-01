f537d0d5750c1c6c6a83c4ac3598faa129a40211
Alessio Pepe <pepealessio.ap@gmail.com>
2022-02-01 10:26:19 +0100

    clean code

--- fp_audio/scripts/audio_detected.py
+++ fp_audio/scripts/audio_detected.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 from config import *
-from fp_audio.srv import StartListening, StopListening
+from fp_audio.srv import StartListening
 import numpy as np
 import rospy
 import speech_recognition as sr
@@ -20,43 +20,46 @@ class AudioDetected(object):
             raw_audio_topic (str): The name of the topic on which the audio must be published.
         """
         self._publisher = rospy.Publisher(raw_audio_topic, Int16MultiArray, queue_size=None)
-        # Initialize a Recognizer
         self._r = sr.Recognizer()
-        # Audio source
         self._m = sr.Microphone(device_index=MICROPHONE_INDEX, sample_rate=RATE, chunk_size=CHUNK_SIZE)
 
         # Mutex because this service can be called from multiple handler, so we need that 
         # thread safe.
         self._mutex = Lock()
-        # self._stop_listening_func = None
-        # self._running = False
+        self._running = False
+
         self._verbose = verbose
     
     def start(self):
         """Start the node and calibrate the microphone to the local noise.
         """
         rospy.init_node('audio_detected_node', anonymous=True)
-        rospy.Service('startListening', StartListening, self._handle_start_listening)
-        # rospy.Service('stopListening', StopListening, self._handle_stop_listening)
-
-        # self.startListening()
-
+        rospy.Service('startListening', StartListening, self._handle_start_listening, buff_size=1)
         rospy.spin()
 
-    def startListening(self):
-        """Calibrate the microphone with the ambient noise and start listening.
+
+    def _handle_start_listening(self, req):
+        """Callback function for startListening service. 
+        Calibrate the microphone with the ambient noise and start listening.
         This method id thread-safe.
+
+        Args:
+            req (StartListening): empty request
+
+        Returns:
+            str: an ack who tell if the audio was listened.
         """
         self._mutex.acquire()
 
+        if not self._running:
+            self._running = True
+            self._mutex.release()
+        else:
+            self._mutex.release()
+            return "already running"
+
         with self._m as source:
-            if self._verbose:
-                print("[T2S] Start calibrating...")
-
             self._r.adjust_for_ambient_noise(source, duration=CALIBRATION_TIME)
-
-            if self._verbose:
-                print("[T2S] Calibrating finished.")
 
             if self._verbose:
                 print("[T2S] Start listened")
@@ -69,43 +72,11 @@ class AudioDetected(object):
             if self._verbose:
                 print("[T2S] Listened")
 
+        self._mutex.acquire()
+        self._running = False
         self._mutex.release()
 
-    # def stopListening(self):
-    #     """Stop the listening from the microphone.
-    #     This method id thread-safe.
-    #     """
-    #     self._mutex.acquire()
-
-    #     if self._running:
-    #         if self._stop_listening_func is not None:
-    #             self._stop_listening_func()
-    #             self._stop_listening_func = None
-    #             if self._verbose:
-    #                 print("[T2S] Stop listening")
-
-    #         self._running = False
-        
-    #     self._mutex.release()
-
-    # def _listened_callback(self, recognizer, audio):
-    #     """Callback function to the listen_in_background function.
-    #     """
-    #     data_to_send = Int16MultiArray()
-    #     data_to_send.data = np.frombuffer(audio.get_raw_data(), dtype=np.int16)
-    #     self._publisher.publish(data_to_send)
-
-    def _handle_start_listening(self, req):
-        """Callback function for startListening service.
-        """
-        self.startListening()
-        return "ACK"
-
-    # def _handle_stop_listening(self, req):
-    #     """Callback function for stopListening service.
-    #     """
-    #     self.stopListening()
-    #     return "ACK"
+        return "listened"
 
 
 if __name__ == "__main__":
